e1bd8c51376addfbd16105c163350a34e8e77006
crimsoncanvas <39696114+crimsoncanvas@users.noreply.github.com>
2018-05-29 13:08:35 +0530

    new crim

--- scripts/form-u2210.php
+++ scripts/form-u2210.php
@@ -11,7 +11,7 @@ require_once('form_process.php');
 $form = array(
 	'subject' => 'Contact Form Submission',
 	'heading' => 'New Form Submission',
-	'success_redirect' => 'index.html',
+	'success_redirect' => '',
 	'resources' => array(
 		'checkbox_checked' => 'Checked',
 		'checkbox_unchecked' => 'Unchecked',
